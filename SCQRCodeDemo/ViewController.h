e9c89f178b4f6d9517dcb7b89a1b22485101d863
chow <stefanchow6@hotmail.com>
2016-03-11 00:01:50 +0800

    Initial commit

---
+++ SCQRCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCQRCodeDemo
+//
+//  Created by chow on 16/3/9.
+//  Copyright © 2016年 chow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
